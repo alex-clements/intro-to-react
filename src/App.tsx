1abf239bcdfa1489aff4904ddf145774e0a20034
alex-clements <ahclemen@ualberta.ca>
2024-03-15 20:51:26 -0700

    Remove completed files

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
 import "./App.css";
-import { useState } from "react";
-import { AddItem } from "./components/AddItem";
-import { ToDoList } from "./components/ToDoList";
 
 function App() {
-  const [toDoList, setToDoList] = useState<string[]>([]);
-
-  const handleUpdateToDoList = (item: string) => {
-    setToDoList([...toDoList, item]);
-  };
-
   return (
     <>
       <h1>Intro to React</h1>
-      <AddItem onUpdateToDoList={handleUpdateToDoList} />
-      <ToDoList toDoList={toDoList} />
     </>
   );
 }
